b301fb7f411207f483a87b15149180fcf0920fed
craigvantonder <craig@compulutions.co.za>
2016-04-23 12:29:42 +0200

    Revised select method into repeating function

--- bash-git-simple-cli.sh
+++ bash-git-simple-cli.sh
@@ -3,7 +3,7 @@
 ###
  # @bash-git-simple-cli A simple Command Line Interface written in BASH and used to maintain Github repositories with Git.
  # @author Craig van Tonder
- # @version 0.0.2
+ # @version 0.0.3
  ##
 
 ##### CONSTANTS
@@ -34,44 +34,177 @@ OPTIONS=(
   "Cancel"
 )
 
-# Start the select loop
-select opt in "${OPTIONS[@]}"
-do
-  case $opt in
-    # ===============
-    # SELECTED COMMIT
-    # ===============
-    "Commit to remote")
-      # Started task
-      echo "=> Committing to remote..."
+# Create a function that shows the menu and echoes the users choice
+# http://stackoverflow.com/a/35854852/2110294
+function show_menu {
+  select option in "${OPTIONS[@]}"; do
+    echo $option
+    break
+  done
+}
 
-      # Define the emailaddress used in conjuction with the SSH key to access github
-      if [ -z "$SSH_KEY" ]
-      then
-        echo -n "Full path of SSH key: "
-        read SSH_KEY
-        USING_DEFAULTS=false
-      fi
+# We can wrap that function in a loop, and only break out if the user picked Cancel
+while true; do
+  option=$(show_menu)
 
-      # Define the emailaddress used in conjuction with the SSH key to access github
-      echo -n "Branch to commit to: "
-      read BRANCH
+  # =========================
+  # SELECTED COMMIT TO REMOTE
+  # =========================
+  if [[ $option == "Commit to remote" ]]; then
+    # Started task
+    echo "=> Committing to remote..."
 
-      # Prompt that defaults were used
-      if [ -z "$USING_DEFAULTS" ]
-      then
-        echo "=> Using default SSH key"
-      fi
+    # Define the emailaddress used in conjuction with the SSH key to access github
+    if [ -z "$SSH_KEY" ]
+    then
+      echo -n "Full path of SSH key: "
+      read SSH_KEY
+      USING_DEFAULTS=false
+    fi
 
-      # Define the select message
-      echo -n "Commit message: "
-      read message
+    # Define the emailaddress used in conjuction with the SSH key to access github
+    echo -n "Branch to commit to: "
+    read BRANCH
 
-      # Add the changes to the index
-      git add -A
+    # Prompt that defaults were used
+    if [ -z "$USING_DEFAULTS" ]
+    then
+      echo "=> Using default SSH key"
+    fi
 
-      # Commit the changes
-      git commit --interactive -m "$message"
+    # Define the select message
+    echo -n "Commit message: "
+    read message
+
+    # Add the changes to the index
+    git add -A
+
+    # Commit the changes
+    git commit --interactive -m "$message"
+
+    # Fork a copy of ssh-agent and generate Bourne shell commands on stdout
+    eval $(ssh-agent -s)
+
+    # Load the ssh key for access to Github
+    ssh-add $SSH_KEY
+
+    # Changes are currently in the HEAD of your local working copy
+    # so send those changes to your remote repository
+    git push origin $BRANCH
+
+    # Kill the ssh-agent process
+    pkill ssh-agent
+
+    # Ended task
+    echo "Successfully committed to remote!"
+    echo "What would you like to do next?"
+  fi
+
+  # =======================
+  # SELECTED MERGE BRANCHES
+  # =======================
+  if [[ $option == "Merge branches" ]]; then
+    # Define the branch to name
+    echo -n "=> Merging to branch: "
+    read TO_BRANCH
+
+    # Define the branch from name
+    echo -n "=> Merging changes from branch: "
+    read FROM_BRANCH
+
+    # Switch to master
+    git checkout $TO_BRANCH
+
+    # Merge the branch into master
+    git merge --squash $FROM_BRANCH
+
+    # Ended task
+    echo "Successfully merged $FROM_BRANCH into $TO_BRANCH"
+    echo "What would you like to do next?"
+  fi
+
+  # ======================
+  # SELECTED SHOW BRANCHES
+  # ======================
+  if [[ $option == "Show branches" ]]; then
+    # Show the branches
+    git show-branch --list
+
+    # Ended task
+    echo "What would you like to do next?"
+  fi
+
+  # ======================
+  # SELECTED CREATE BRANCH
+  # ======================
+  if [[ $option == "Create branch" ]]; then
+    # Define the branch name
+    echo -n "=> Branch to create: "
+    read BRANCH
+
+    # Create the branch
+    git checkout -b $BRANCH
+
+    # Ended task
+    echo "What would you like to do next?"
+  fi
+
+  # ======================
+  # SELECTED SWITCH BRANCH
+  # ======================
+  if [[ $option == "Switch to branch" ]]; then
+    # Define the branch name
+    echo -n "=> Branch to switch to: "
+    read BRANCH
+
+    # Switch to the branch
+    git checkout $BRANCH
+
+    # Ended task
+    echo "What would you like to do next?"
+  fi
+
+  # ======================
+  # SELECTED DELETE BRANCH
+  # ======================
+  if [[ $option == "Delete branch" ]]; then
+    # Define the branch name
+    echo -n "=> Branch name to delete: "
+    read BRANCH
+
+    echo -n "=> Delete local branch: \"$BRANCH\"? (y/n): "
+    read DELETE_LOCAL_BRANCH
+    echo -n "=> Delete remote branch: \"$BRANCH\"? (y/n): "
+    read DELETE_REMOTE_BRANCH
+
+    # Define the email address used in conjuction with the SSH key to access github
+    if [ -z "$SSH_KEY" ]
+    then
+      echo -n "=> Full path of SSH key: "
+      read SSH_KEY
+      USING_DEFAULTS=false
+    fi
+
+    # Prompt that defaults were used
+    if [ -z "$USING_DEFAULTS" ]
+    then
+      echo "=> Using default SSH key"
+    fi
+
+    # If we are going to delete the local branch
+    if [ $DELETE_LOCAL_BRANCH == "y" ]
+    then
+      # Prompt that the task was cancelled
+      echo "=> Deleting local branch..."
+      # Delete the local branch
+      git branch -d $BRANCH
+    fi
+
+    # If we are going to delete the remote branch
+    if [ $DELETE_REMOTE_BRANCH == "y" ]
+    then
+      # Prompt that the task was cancelled
+      echo "=> Deleting remote branch..."
 
       # Fork a copy of ssh-agent and generate Bourne shell commands on stdout
       eval $(ssh-agent -s)
@@ -79,224 +212,102 @@ do
       # Load the ssh key for access to Github
       ssh-add $SSH_KEY
 
-      # Changes are currently in the HEAD of your local working copy
-      # so send those changes to your remote repository
-      git push origin $BRANCH
+      # Delete the remote branch
+      git push origin --delete $BRANCH
 
       # Kill the ssh-agent process
       pkill ssh-agent
+    fi
 
-      # Ended task
-      echo "=> Committed to remote"
-
-      break
-    ;;
-
-    # ==============
-    # MERGE BRANCHES
-    # ==============
-    "Merge branches")
-
-      # Define the branch to name
-      echo -n "=> Merging to branch: "
-      read TO_BRANCH
-
-      # Define the branch from name
-      echo -n "=> Merging changes from branch: "
-      read FROM_BRANCH
-
-      # Switch to master
-      git checkout $TO_BRANCH
-
-      # Merge the branch into master
-      git merge --squash $FROM_BRANCH
-
-      # Ended task
-      echo "=> Successfully merged $FROM_BRANCH into $TO_BRANCH"
-
-      break
-    ;;
-
-    # ======================
-    # SELECTED SHOW BRANCHES
-    # ======================
-    "Show branches")
-
-      # Show the branches
-      git show-branch --list
-
-      break
-    ;;
-
-    # ======================
-    # SELECTED CREATE BRANCH
-    # ======================
-    "Create branch")
-
-      # Define the branch name
-      echo -n "=> Branch to create: "
-      read BRANCH
-
-      # Create the branch
-      git checkout -b $BRANCH
-
-      break
-    ;;
-
-    # ======================
-    # SELECTED SWITCH BRANCH
-    # ======================
-    "Switch to branch")
-
-      # Define the branch name
-      echo -n "=> Branch to switch to: "
-      read BRANCH
-
-      # Switch to the branch
-      git checkout $BRANCH
-
-      break
-    ;;
-
-    # ======================
-    # SELECTED DELETE BRANCH
-    # ======================
-    "Delete branch")
-
-      # Define the branch name
-      echo -n "=> Branch name to delete: "
-      read BRANCH
-
-      echo -n "=> Delete local branch: \"$BRANCH\"? (y/n): "
-      read DELETE_LOCAL_BRANCH
-      echo -n "=> Delete remote branch: \"$BRANCH\"? (y/n): "
-      read DELETE_REMOTE_BRANCH
-
-      # Define the emailaddress used in conjuction with the SSH key to access github
-      if [ -z "$SSH_KEY" ]
-      then
-        echo -n "=> Full path of SSH key: "
-        read SSH_KEY
-        USING_DEFAULTS=false
-      fi
-
-      # Prompt that defaults were used
-      if [ -z "$USING_DEFAULTS" ]
-      then
-        echo "=> Using default SSH key"
-      fi
-
-      # If we are going to delete the local branch
-      if [ $DELETE_LOCAL_BRANCH == "y" ]
-      then
-        # Prompt that the task was cancelled
-        echo "=> Deleting local branch..."
-        # Delete the local branch
-        git branch -d $BRANCH
-      fi
-
-      # If we are going to delete the remote branch
-      if [ $DELETE_REMOTE_BRANCH == "y" ]
-      then
-        # Prompt that the task was cancelled
-        echo "=> Deleting remote branch..."
-
-        # Fork a copy of ssh-agent and generate Bourne shell commands on stdout
-        eval $(ssh-agent -s)
-
-        # Load the ssh key for access to Github
-        ssh-add $SSH_KEY
-
-        # Delete the remote branch
-        git push origin --delete $BRANCH
-
-        # Kill the ssh-agent process
-        pkill ssh-agent
-      fi
-
-      # If both options were no
-      if [ $DELETE_LOCAL_BRANCH == "n" ] && [ $DELETE_REMOTE_BRANCH == "n" ]
-      then
-        # Prompt that the task was cancelled
-        echo "=> Cancelled "
-      fi
-
-      break
-    ;;
-
-    # ==========================
-    # SELECTED INITIALISE CONFIG
-    # ==========================
-    "Initialise .git/config")
-      # Started task
-      echo "=> Initialising .git/config..."
-
-      # Define the name used to create the repository on Github
-      if [ -z "$REPO_NAME" ]
-      then
-        echo -n "Repository name: "
-        read REPO_NAME
-        # Trigger using defaults prompt
-        USING_DEFAULTS=false
-      fi
-
-      # Define the username used in conjuction with the email address for Github collaborators
-      if [ -z "$USER_NAME" ]
-      then
-        echo -n "Github username: "
-        read USER_NAME
-        USING_DEFAULTS=false
-      fi
-
-      # Define the emailaddress used in conjuction with the SSH key to access github
-      if [ -z "$USER_EMAIL" ]
-      then
-        echo -n "Email address: "
-        read USER_EMAIL
-        USING_DEFAULTS=false
-      fi
-
-      # Prompt that defaults were used
-      if [ -z "$USING_DEFAULTS" ]
-      then
-        echo "=> Using constant defaults"
-      fi
-
-      # Echo in the basic configuration for git to use
-      echo "[core]
-        repositoryformatversion = 0
-        filemode = true
-        bare = false
-        logallrefupdates = true
-[remote \"origin\"]
-        url = git@github.com:$USER_NAME/$REPO_NAME.git
-        fetch = +refs/heads/*:refs/remotes/origin/*
-[branch \"master\"]
-        remote = origin
-        merge = refs/heads/master
-[user]
-        name = $USER_NAME
-        email = $USER_EMAIL" > .git/config
-
-      # Ended task
-      echo "=> Initialised .git/config"
-
-      break
-    ;;
-
-    # ===============
-    # SELECTED CANCEL
-    # ===============
-    "Cancel")
+    # If both options were no
+    if [ $DELETE_LOCAL_BRANCH == "n" ] && [ $DELETE_REMOTE_BRANCH == "n" ]
+    then
       # Prompt that the task was cancelled
       echo "=> Cancelled "
+    fi
 
-      break
-    ;;
+    # Ended task
+    echo "What would you like to do next?"
+  fi
 
-    # INPUT WAS NOT WITHIN RANGE OF OPTIONS
-    *)
-      echo "Invalid option selected!"
-    ;;
-  esac
+  # ==========================
+  # SELECTED INITIALISE CONFIG
+  # ==========================
+  if [[ $option == "Initialise .git/config" ]]; then
+    # Started task
+    echo "=> Initialising .git/config..."
+
+    # Define the name used to create the repository on Github
+    if [ -z "$REPO_NAME" ]
+    then
+      echo -n "Repository name: "
+      read REPO_NAME
+      # Trigger using defaults prompt
+      USING_DEFAULTS=false
+    fi
+
+    # Define the username used in conjuction with the email address for Github collaborators
+    if [ -z "$USER_NAME" ]
+    then
+      echo -n "Github username: "
+      read USER_NAME
+      USING_DEFAULTS=false
+    fi
+
+    # Define the emailaddress used in conjuction with the SSH key to access github
+    if [ -z "$USER_EMAIL" ]
+    then
+      echo -n "Email address: "
+      read USER_EMAIL
+      USING_DEFAULTS=false
+    fi
+
+    # Prompt that defaults were used
+    if [ -z "$USING_DEFAULTS" ]
+    then
+      echo "=> Using constant defaults"
+    fi
+
+    # Echo in the basic configuration for git to use
+    echo "[core]
+      repositoryformatversion = 0
+      filemode = true
+      bare = false
+      logallrefupdates = true
+[remote \"origin\"]
+      url = git@github.com:$USER_NAME/$REPO_NAME.git
+      fetch = +refs/heads/*:refs/remotes/origin/*
+[branch \"master\"]
+      remote = origin
+      merge = refs/heads/master
+[user]
+      name = $USER_NAME
+      email = $USER_EMAIL" > .git/config
+
+    # Ended task
+    echo "=> Initialised .git/config"
+    echo "What would you like to do next?"
+  fi
+
+  # ===============
+  # SELECTED CANCEL
+  # ===============
+  if [[ $option == "Cancel" ]]; then
+    break
+  fi
+
+  # =====================================
+  # INPUT WAS NOT WITHIN RANGE OF OPTIONS
+  # =====================================
+  if [[ !$option == "Commit to remote" &&
+        !$option == "Merge branches" &&
+        !$option == "Show branches" &&
+        !$option == "Create branch" &&
+        !$option == "Switch to branch" &&
+        !$option == "Delete branch" &&
+        !$option == "Initialise .git/config" &&
+        !$option == "Cancel" ]]; then
+    echo "Invalid option selected!"
+  fi
+
 done
